decf697e8929e261ebead873d01609c7a1d217ce
bala-amypo <bala@amypo.in>
2025-12-26 16:26:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/LocationRepository.java
+++ src/main/java/com/example/demo/repository/LocationRepository.java
@@ -1,11 +1,4 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
 public interface LocationRepository extends JpaRepository<Location, Long> {
-
     Optional<Location> findByLocationName(String name);
+    List<Location> findByRegion(String region);
 }
--- src/main/java/com/example/demo/repository/SensorReadingRepository.java
+++ src/main/java/com/example/demo/repository/SensorReadingRepository.java
@@ -1,19 +1,4 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.SensorReading;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface SensorReadingRepository
-        extends JpaRepository<SensorReading, Long> {
-
+public interface SensorReadingRepository extends JpaRepository<SensorReading, Long> {
     List<SensorReading> findBySensor_Id(Long id);
-
-    List<SensorReading> findBySensor_IdAndReadingTimeBetween(
-            Long id,
-            LocalDateTime start,
-            LocalDateTime end
-    );
+    List<SensorReading> findBySensor_IdAndReadingTimeBetween(Long id, LocalDateTime s, LocalDateTime e);
 }
